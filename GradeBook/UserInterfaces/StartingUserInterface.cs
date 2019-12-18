39f5f799fd54c32ab2e4c10ec546400bba62c187
Nichola Kelly <55988653+nicholakelly@users.noreply.github.com>
2019-12-18 15:22:10 +0000

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,13 +42,14 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
+
             if (type == "standard")
                 gradeBook = new BaseGradeBook(name);
             else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a supported type of gradebook,please try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.",type);
                 return;
             }
             
